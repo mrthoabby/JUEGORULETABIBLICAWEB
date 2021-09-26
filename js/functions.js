83fd66214faed6f3ffab89711e18ce8a4996bb98
danis abadia <deanis89@hotmail.com>
2021-09-26 15:46:56 -0500

    first-part of game eviroment

--- js/functions.js
+++ js/functions.js
@@ -1,15 +1,16 @@
 //Variables Locales
-
-
+var playersDemos = {1:"MrthoAbby",2:"JugadordePrueba",3:"JugadordePrueba",4:"JugadordePrueba",5:"Demo",6:"JugadordePrueba",7:"JugadordePrueba",8:"JugadordePrueba",9:"JugadordePrueba",10:"JugadordePrueba"};
+var quantitysquares = 100;
 
 
 //Variables localizando elementos
 var numberPlayers = document.getElementById('numberPlayers');
 const playersSelector = document.getElementById('playersSelector');
-
+var nameplayers = document.getElementById('name-players');
+var antionGame = document.getElementById('action-game');
 
 //Functions
-function AssignNumber(){
+function assignNumber(){
     document.getElementById('number-players').innerHTML = numberPlayers.value;
 }
 function createPlayers(){
@@ -32,11 +33,39 @@ function createPlayers(){
         butonPlay.setAttribute('id','butonPlay');
         playersSelector.appendChild(butonPlay);
 }
+function showPlayers(){
+    let titleCurrentplayers = document.createElement('H4');
+        titleCurrentplayers.setAttribute('class','title-players');
+        titleCurrentplayers.textContent = "Jugadores"
+        nameplayers.appendChild(titleCurrentplayers);
+    for (let player in playersDemos) {
+        let currentPlayer = document.createElement("ARTICLE");
+        let nameplayer = playersDemos[player];
+        currentPlayer.setAttribute('id','p'+player);
+        currentPlayer.setAttribute('class','player');
+        currentPlayer.innerHTML = `<strong class="namep">${playersDemos[player]}</strong><p class="infop">Dinero acumulado: $0</p> <span class="numberp">${player}</span>`;
+        nameplayers.appendChild(currentPlayer);
+    }
+              
+}
+
+function generateSquare(){
+    for (let index = 0; index < quantitysquares; index++) {
+        let square = document.createElement("DIV");
+        square.setAttribute('id','S'+index);
+        square.setAttribute('class','square');
+        antionGame.appendChild(square);
+        
+    }
+
+}
 
 
 
 
 //Events
-numberPlayers.addEventListener('change',AssignNumber);
-window.addEventListener('load',AssignNumber);
-window.addEventListener('load',createPlayers);
+numberPlayers.addEventListener('change',assignNumber);
+// window.addEventListener('load',assignNumber);
+// window.addEventListener('load',createPlayers);
+window.addEventListener('load',showPlayers);
+window.addEventListener('load',generateSquare);
