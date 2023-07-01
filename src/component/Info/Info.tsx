39dfd7e8a1775d8f22f8e67aeb7c18dbfe692d7f
Danis Abadia <deanis89@hotmail.com>
2023-06-30 19:37:11 -0500

    REFACTOR(Info) End component

--- src/component/Info/Info.tsx
+++ src/component/Info/Info.tsx
@@ -1,13 +1,22 @@
 import styles from "./styles.module.css"
 type InfoProps = {
     infoAction: string;
+    description?: string;
+    date: Date;
+    player?: string;
 };
 
-export const Info = ({ infoAction }: InfoProps) => {
+export const Info = ({ infoAction, description, date, player }: InfoProps) => {
     return (
         <section className={styles.container}>
-            <span className={styles.date}>{new Date().toLocaleDateString(undefined, { hour: '2-digit', minute: '2-digit', second: '2-digit' })}</span>
-            <span className={styles.info}>  {infoAction}</span>
+            <span className={styles.date}>{date.toLocaleDateString(undefined, { hour: '2-digit', minute: '2-digit', second: '2-digit' })}</span>
+            <span className={styles.info}>
+                <b>
+                    {infoAction}:
+                </b>
+                <i>{description}</i>
+                <small>{`[@${player}]`}</small>
+            </span>
         </section>
     );
 }
